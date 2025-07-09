59ef3583d13a03c2d97de672242153c77e5fba55
Alfonsomcb <amcb1997@gmail.com>
2025-07-09 14:37:25 -0700

    Creción del repositorio y primeros archivos

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # proyecto_sprint_7
 Repositorio para proyecto del sprint 7 Triple10 (Alfonso Castro)
+
+Este repositorio tiene el propósito de crear una app web por medio de streamlit y hosteado por render.com donde utilizamos un dataset y analizamos aspectos basicos de su contenido 
+
+Librerias a instalar
+(Puedes leer el archivo requirements.txt)
+
+Estructura
+1.- Main folder
+    app.py
+    requirements.txt
+    README.md
+    vehicles_us (1).csv
+
+    1.1 Notebooks
+        EDA.ipynb
+
+
+Contexto
+
+app.py (corre la aplicación web)
+requirements.txt (incluye las librerias que se deben de instalar para correr app.py)
+README.md (explicación e introducción al repositorio)
+vehicles_us (1).csv (dataset con el que trabajamos)
+EDA.ipyb (se utilizó como laboratorio de código)
+
+
---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Load the dataset
+car_data = pd.read_csv("vehicles_us (1).csv")
+
+# Streamlit app title
+st.title('Análisis Exploratorio de Datos de Anuncios de Venta de Coches')
+
+# Display the first few rows of the dataset
+st.subheader('Primeras filas del conjunto de datos')
+st.dataframe(car_data.head())
+
+# Histogram button
+st.subheader('Visualización de Datos')
+st.write('Seleccione una opción para visualizar los datos:')
+
+hist_checkbox = st.checkbox('Construir histograma') # crear un botón
+     
+if hist_checkbox: # al hacer clic en el botón
+     # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+     # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+#scatter button
+st.subheader('Visualización de Datos')
+st.write('Seleccione una opción para visualizar los datos:')
+
+scatter_checkbox = st.checkbox('Construir Dispersión') # crear un botón
+     
+if scatter_checkbox: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+         
+    # crear un histograma
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)    
